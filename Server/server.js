831d2a58cf1b1c33af2b4a1317a471ce42044e29
Shashank Lakhera <lakherashashank70@gmail.com>
2026-07-12 07:49:43 +0530

    feat: initialize backend project with Express, Mongoose, and environment configuration

--- Server/server.js
+++ Server/server.js
@@ -1,11 +1,14 @@
-import express from 'express'
+import dotenv from "dotenv";
+dotenv.config();
+import app from "./src/app.js";
+import connectDB from "./src/config/db.js";
+import { env } from "./src/config/env.js";
 
-const app = express()
-
-function startServer() {
-    app.listen(3000, () => {
-        console.log(`Server is running at 3000`);
-    })
+async function startServer() {
+  await connectDB();
+  app.listen(env.PORT, () => {
+    console.log(`Server is running at ${env.PORT}`);
+  });
 }
 
 startServer();
